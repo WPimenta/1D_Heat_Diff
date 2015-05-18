4cb4d2bc186c1fd85a160122644a76acb1ba3250
TipTeQ <tipteq@gmail.com>
2015-05-18 15:31:56 +0200

    Finished with Cuda
    Added functionality for reading from and writing to files

--- heat.cu
+++ heat.cu
@@ -1,13 +1,18 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <cuda_runtime.h>
-#define NUMPOINTS 10
-#define ENDTIME 5
-#define DT 0.1
+#define CUDA_INPUT "cuda_input.txt"
+#define CUDA_OUTPUT "cuda_output.txt"
 
-void InitialiseToZero(float* array, int size);
+int NUMPOINTS;
+double ENDTIME;
+double DT;
+double ENDVALUES;
+
+void InitialiseToZero(float* array);
 __device__ void PrintPointsGPU(float* array, int size, double currentTime);
-void PrintPointsCPU(float* array, int size, double currentTime);
+void PrintPointsCPU(float* array, double currentTime);
+void ProcessOutput(float* array, int testCase, float time);
 
 __global__ void DiffuseHeat(float* currentPoints, float* nextPoints, int size, double dx, double dt, double endTime)
 {
@@ -16,10 +21,6 @@ __global__ void DiffuseHeat(float* currentPoints, float* nextPoints, int size, d
 	currentTime = 0.0;
 	while (currentTime < endTime)
 	{
-//		if (threadIndex == 1)
-//		{
-//			PrintPointsGPU(currentPoints, NUMPOINTS, currentTime);
-//		}
 		nextPoints[threadIndex] = currentPoints[threadIndex] + 0.25*(currentPoints[threadIndex+1] - (2*currentPoints[threadIndex]) + currentPoints[threadIndex-1]);
 		__syncthreads();
 		currentPoints[threadIndex] = nextPoints[threadIndex];
@@ -32,40 +33,75 @@ __global__ void DiffuseHeat(float* currentPoints, float* nextPoints, int size, d
 }
 int main(void)
 {
-	float* currentPoints = 0;
-	currentPoints = (float*)malloc(NUMPOINTS*sizeof(float));
-	float* nextPoints = 0;
-	nextPoints = (float*)malloc(NUMPOINTS*sizeof(float));
-	float* resultPoints = 0;
-	resultPoints = (float*)malloc(NUMPOINTS*sizeof(float));
-	float* deviceCurrentPoints = 0;
-	cudaMalloc((void**)&deviceCurrentPoints, NUMPOINTS*sizeof(float));
-	float* deviceNextPoints = 0;
-	cudaMalloc((void**)&deviceNextPoints, NUMPOINTS*sizeof(float));
-	if(currentPoints == 0 || nextPoints == 0 || resultPoints == 0 || deviceCurrentPoints == 0 || deviceNextPoints == 0)
-	{
-		printf("Couldn't allocate memory\n");
-		return 1;
+	remove(CUDA_OUTPUT);
+	int testCase = 1;
+	char str[70];
+	FILE *p;
+	if((p=fopen(CUDA_INPUT,"r"))==NULL){
+		printf("!!!Unable to open file cuda_input.txt!!!");
+		exit(1);
 	}
-	InitialiseToZero(currentPoints, NUMPOINTS);
-	InitialiseToZero(nextPoints, NUMPOINTS);
-	//make the end points some random values
-	float randomValue = rand()%100;
-	currentPoints[0] = randomValue;
-	currentPoints[NUMPOINTS-1] = randomValue;
-	cudaMemcpy(deviceCurrentPoints, currentPoints, NUMPOINTS*sizeof(float), cudaMemcpyHostToDevice);
-	cudaMemcpy(deviceNextPoints, nextPoints, NUMPOINTS*sizeof(float), cudaMemcpyHostToDevice);
-	const size_t blockSize = NUMPOINTS-2;
-	size_t gridSize = (NUMPOINTS-2) / blockSize;
-	double DX = currentPoints[1] - currentPoints[0];
-	DiffuseHeat<<<gridSize, blockSize>>>(deviceCurrentPoints, deviceNextPoints, NUMPOINTS, DX, DT, ENDTIME);
-	cudaMemcpy(resultPoints, deviceCurrentPoints, NUMPOINTS*sizeof(float), cudaMemcpyDeviceToHost);
-	PrintPointsCPU(resultPoints, NUMPOINTS, ENDTIME);
+	while(fgets(str,70,p)!=NULL)
+	{
+		//File is in format: NUMPOINTS ENDTIME DT ENDVALUES
+		char * pch;
+		pch = strtok (str," ");
+		NUMPOINTS = atof(pch);
+		pch = strtok (NULL, " ");
+		ENDTIME = atof(pch);
+		pch = strtok (NULL, " ");
+		DT = atof(pch);
+		pch = strtok (NULL, " ");
+		ENDVALUES = atof(pch);
+
+		float* currentPoints = 0;
+		currentPoints = (float*)malloc(NUMPOINTS*sizeof(float));
+		float* nextPoints = 0;
+		nextPoints = (float*)malloc(NUMPOINTS*sizeof(float));
+		float* resultPoints = 0;
+		resultPoints = (float*)malloc(NUMPOINTS*sizeof(float));
+		float* deviceCurrentPoints = 0;
+		cudaMalloc((void**)&deviceCurrentPoints, NUMPOINTS*sizeof(float));
+		float* deviceNextPoints = 0;
+		cudaMalloc((void**)&deviceNextPoints, NUMPOINTS*sizeof(float));
+		if(currentPoints == 0 || nextPoints == 0 || resultPoints == 0 || deviceCurrentPoints == 0 || deviceNextPoints == 0)
+		{
+			printf("Couldn't allocate memory\n");
+			return 1;
+		}
+		InitialiseToZero(currentPoints);
+		InitialiseToZero(nextPoints);
+
+		float randomValue = ENDVALUES;
+		currentPoints[0] = ENDVALUES;
+		currentPoints[NUMPOINTS-1] = randomValue;
+		cudaMemcpy(deviceCurrentPoints, currentPoints, NUMPOINTS*sizeof(float), cudaMemcpyHostToDevice);
+		cudaMemcpy(deviceNextPoints, nextPoints, NUMPOINTS*sizeof(float), cudaMemcpyHostToDevice);
+		const size_t blockSize = NUMPOINTS-2;
+		size_t gridSize = (NUMPOINTS-2) / blockSize;
+		double DX = currentPoints[1] - currentPoints[0];
+
+		cudaEvent_t launch_begin, launch_end;
+		cudaEventCreate(&launch_begin);
+		cudaEventCreate(&launch_end);
+		cudaEventRecord(launch_begin,0);
+		DiffuseHeat<<<gridSize, blockSize>>>(deviceCurrentPoints, deviceNextPoints, NUMPOINTS, DX, DT, ENDTIME);
+		cudaEventRecord(launch_end,0);
+		cudaEventSynchronize(launch_end);
+		float time = 0;
+		cudaEventElapsedTime(&time, launch_begin, launch_end);
+
+		cudaMemcpy(resultPoints, deviceCurrentPoints, NUMPOINTS*sizeof(float), cudaMemcpyDeviceToHost);
+
+		ProcessOutput(resultPoints, testCase, time);
+		testCase++;
+	}
+	fclose(p);
 	return 0;
 }
-void InitialiseToZero(float* array, int size)
+void InitialiseToZero(float* array)
 {
-	for (int index = 0; index < size; index++)
+	for (int index = 0; index < NUMPOINTS; index++)
 	{
 		array[index] = 0;
 	}
@@ -81,12 +117,33 @@ __device__ void PrintPointsGPU(float* array, int size, double currentTime)
 	printf("\n\n");
 }
 
-void PrintPointsCPU(float* array, int size, double currentTime)
+void PrintPointsCPU(float* array, double currentTime)
 {
 	printf("The array values at time t=%0.1f are:\n", currentTime);
-	for (int index = 0; index < size; index++)
+	for (int index = 0; index < NUMPOINTS; index++)
 	{
 		printf("%0.2f ", array[index]);
 	}
 	printf("\n\n");
+}
+
+void ProcessOutput(float* array, int testCase, float time)
+{
+	FILE *f = fopen(CUDA_OUTPUT, "a");
+	if (f == NULL)
+	{
+		printf("!!!Error opening the output file!!!\n");
+		exit(1);
+	}
+	fprintf(f, "Runtime for test case %d with %d points:\n", testCase, NUMPOINTS);
+	fprintf(f, "%f\n", time);
+	if (NUMPOINTS <= 15) {
+		fprintf(f, "Resultant temperatures:\n");
+		for (int index = 0; index < NUMPOINTS; index++)
+		{
+			fprintf(f, "%0.2f ", array[index]);
+		}
+	}
+	fprintf(f, "\n\n");
+	fclose(f);
 }
